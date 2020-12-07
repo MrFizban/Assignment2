0cc279949ea1c7cba90145cbf40b436a6214e0cf
Giulio Aogostini <fizban94@gmail.com>
2020-12-07 08:26:31 +0100

    ultimo updata

--- query/query_11.sql
+++ query/query_11.sql
@@ -1,6 +1,3 @@
-SELECT  st.city
-FROM "Match" as mt, "Stadium" as st, "Nationality" as nt
-WHERE ((mt.home = nt.country) OR (mt.guest = nt.country)) AND (st.name = mt.stadium) AND
-(nt.group = 'A' OR nt.group = 'B' OR nt.group = 'C')
-GROUP BY st.city
-ORDER BY city ASC
+SELECT  nt.group, nt.continent
+FROM "Nationality" as nt
+ORDER BY  nt.group ASC
